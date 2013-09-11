74e797781c215e5060a24a1f9c4034440b4cb228
Kay Bothfeld <bothfeld@scio.de>
2013-09-11 14:44:09 +0200

    Using reflection to instantiate CurrentBundleVersion; No need for having a dummy class file present at very first start.

--- BundleVersionChecker.cs
+++ BundleVersionChecker.cs
@@ -2,7 +2,9 @@
 // Copyright 2013 by SCIO System-Consulting GmbH & Co. KG. All rights reserved.
 using UnityEngine;
 using UnityEditor;
+using System;
 using System.IO;
+using System.Reflection;
 
 [InitializeOnLoad]
 public class BundleVersionChecker
@@ -15,26 +17,34 @@ public class BundleVersionChecker
 	const string TargetCodeFile = "Assets/Scripts/Config/" + ClassName + ".cs";
 	
 	static BundleVersionChecker () {
-//		string bundleVersion = PlayerSettings.bundleVersion;
-//		string lastVersion = CurrentBundleVersion.version;
-//		if (lastVersion != bundleVersion) {
-//			Debug.Log ("Found new bundle version " + bundleVersion + " replacing code from previous version " + lastVersion +" in file \"" + TargetCodeFile + "\"");
-//			CreateNewBuildVersionClassFile (bundleVersion);
-//		}
+		string bundleVersion = PlayerSettings.bundleVersion;
+		Assembly assembly = Assembly.Load ("Assembly-CSharp");
+		Type type = assembly.GetType (ClassName);
+		if (type != null) {
+			System.Object lastVersionObject = Activator.CreateInstance (type);
+			FieldInfo fieldInfo = type.GetField ("version");
+			string lastVersion = (string)fieldInfo.GetValue (lastVersionObject);
+			if (lastVersion != bundleVersion) {
+				Debug.Log ("Found new bundle version " + bundleVersion + " replacing code from previous version " + lastVersion +" in file \"" + TargetCodeFile + "\"");
+				CreateNewBuildVersionClassFile (bundleVersion);
+			}
+		} else {
+			Debug.Log ("Very first call creating file \"" + TargetCodeFile + "\"" + " for bundle version " + bundleVersion);
+			CreateNewBuildVersionClassFile (bundleVersion);
+		}
 	}
 
-	static string CreateNewBuildVersionClassFile (string bundleVersion) {
+	static void CreateNewBuildVersionClassFile (string bundleVersion) {
 		using (StreamWriter writer = new StreamWriter (TargetCodeFile, false)) {
 			try {
 				string code = GenerateCode (bundleVersion);
 				writer.WriteLine ("{0}", code);
 			} catch (System.Exception ex) {
-				string msg = " threw:\n" + ex.ToString ();
+				string msg = " \n" + ex.ToString ();
 				Debug.LogError (msg);
 				EditorUtility.DisplayDialog ("Error when trying to regenerate class", msg, "OK");
 			}
 		}
-		return TargetCodeFile;
 	}
 	
 	/// <summary>
@@ -47,8 +57,8 @@ public class BundleVersionChecker
 	/// New bundle version.
 	/// </param>
 	static string GenerateCode (string bundleVersion) {
-		string code = "public static class " + ClassName + "\n{\n";
-		code += System.String.Format ("\tpublic static readonly string version = \"{0}\";", bundleVersion);
+		string code = "public class " + ClassName + "\n{\n";
+		code += System.String.Format ("\tpublic string version = \"{0}\";", bundleVersion);
 		code += "\n}\n";
 		return code;
 	}
